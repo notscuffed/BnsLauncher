28775fe3fc3d9a127d540cc5096e41ae7669f9f6
notscuffed <47509036+notscuffed@users.noreply.github.com>
2020-06-06 23:50:08 +0200

    Added logging from client processes using named pipes

--- Source/BnsLauncher.Core/Services/GameStarter.cs
+++ Source/BnsLauncher.Core/Services/GameStarter.cs
@@ -24,38 +24,61 @@ namespace BnsLauncher.Core.Services
                     "/LaunchByLauncher",
                     "/LoginMode", "2"
                 };
-                
+
                 if (gameConfig.Unattended)
                     args.Add("-UNATTENDED");
-                
+
                 if (gameConfig.NoTextureStreaming)
                     args.Add("-NOTEXTURESTREAMING");
-                
+
                 if (gameConfig.UseAllCores)
                     args.Add("-USEALLAVAILABLECORES");
 
+                var pipeName = "bns_" + Guid.NewGuid().ToString().Replace("-", "");
+
                 Environment.SetEnvironmentVariable("__COMPAT_LAYER", "RUNASINVOKER");
                 Environment.SetEnvironmentVariable("BNS_PROFILE_XML", profile.ProfilePath);
+                Environment.SetEnvironmentVariable("BNS_LOG", pipeName);
 
                 var arguments = string.Join(" ", args);
 
                 _logger.Log($"Starting game with arguments: {arguments}");
-                
+                _logger.Log($"Log pipe name: {pipeName}");
+
                 var process = new Process
                 {
                     StartInfo =
                     {
                         FileName = gameConfig.ClientPath,
                         Arguments = arguments,
-                        UseShellExecute = false
+                        UseShellExecute = false,
+                        RedirectStandardOutput = true
                     },
-                    EnableRaisingEvents = true
+                    EnableRaisingEvents = true,
                 };
 
-                process.Exited += (sender, eventArgs) => profile.RemoveProcess(process);
+                var namedPipeToLog = new NamedPipeToLog(_logger, pipeName);
 
-                process.Start();
+                process.Exited += (sender, eventArgs) =>
+                {
+                    profile.RemoveProcess(process);
+                    namedPipeToLog.Close();
 
+                    _logger.Log($"Process {process.Id} has exited");
+                };
+                process.OutputDataReceived += (sender, eventArgs) =>
+                {
+                    if (string.IsNullOrWhiteSpace(eventArgs.Data))
+                        return;
+
+                    _logger.Log(eventArgs.Data);
+                };
+
+                if (!process.Start())
+                    return;
+
+                namedPipeToLog.LogPrefix = $"(Pid: {process.Id}) ";
+                namedPipeToLog.StartLogging();
                 profile.AddProcess(process);
             }
             catch (Exception exception)
