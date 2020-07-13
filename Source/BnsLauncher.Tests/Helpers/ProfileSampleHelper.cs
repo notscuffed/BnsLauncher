0e74564076259a35f09981ddbcab02e00e500fdd
notscuffed <47509036+notscuffed@users.noreply.github.com>
2020-07-13 02:01:06 +0200

    Added tests

---
+++ Source/BnsLauncher.Tests/Helpers/ProfileSampleHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.IO.Abstractions.TestingHelpers;
+
+namespace BnsLauncher.Tests.Helpers
+{
+    public static class ProfileSampleHelper
+    {
+        static ProfileSampleHelper()
+        {
+            SolutionDirectory = AppContext.BaseDirectory.Split(
+                new[] {"BnsLauncher.Tests"},
+                StringSplitOptions.RemoveEmptyEntries)[0];
+        }
+
+        public static string SolutionDirectory { get; }
+        
+        public static void WriteSampleProfile(this MockFileSystem fs, string testProfileRoot, string profile)
+        {
+            fs.Directory.CreateDirectory(testProfileRoot);
+            
+            var sampleDirectory = $"{SolutionDirectory}BnsLauncher.Core\\Samples\\{profile}";
+
+            foreach (var file in Directory.EnumerateFiles(sampleDirectory))
+            {
+                var content = File.ReadAllBytes(file);
+                fs.File.WriteAllBytes($"{testProfileRoot}\\{Path.GetFileName(file)}", content);
+            }
+        }
+    }
+}
